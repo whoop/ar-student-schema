f33e8294dd0bceae363885c121fa091b455ca1de
Ian Loo <loohanrong@gmail.com>
2015-09-28 09:39:18 +0800

    Changed validates :uniqueness to validates_uniqueness_of

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,6 @@
 require_relative '../../db/config'
 
 class Teacher < ActiveRecord::Base
-  validates :email, :uniqueness => { :message => '%{value} is not unique' }
+  validates_uniqueness_of :email, message: '%{value} is not unique'
   has_and_belongs_to_many :students
 end
